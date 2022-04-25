6ce15a832724ed816c89245fcdf478632b78c68a
catgok <catgok@163.com>
2022-04-26 00:04:17 +0800

    22/4/25 23.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import router from "@/router";
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
+import '../static/css/tmp.css'
 
 Vue.use(ElementUI);
 
--- src/views/tmp.vue
+++ src/views/tmp.vue
@@ -1,20 +1,23 @@
 <template>
   <div>
-    <div>tmp</div>
+    <div>清分计算</div>
     <div class="input">
       <div v-for="(item,index) in input_information" :key="index">
         <div>
           <el-input class="input-name"
-                    placeholder="请输入收益方名称"
+                    placeholder="收益方名称"
                     v-model="item.input_name"
                     maxlength="20"
                     show-word-limit>
           </el-input>
           <el-input class="input-percent"
-                    placeholder="请输入收益方收益比例(小数)"
+                    placeholder="收益比例"
                     v-model="item.input_percent"
                     show-word-limit>
+            <!--            eslint-disable -->
+            <template slot="append">%</template>
           </el-input>
+
           <button type="button" @click="downObj(item)">删除</button>
           <button v-if="index===input_information.length-1" type="button" @click="addObj()">添加</button>
         </div>
@@ -23,7 +26,7 @@
       <button type="button" @click="clac()">开始计算</button>
     </div>
 
-    <div class="pie" style="width:40%">
+    <div class="pie">
       <canvas id="pieChart"></canvas>
     </div>
 
@@ -69,25 +72,32 @@ export default {
       let sum = 0, error_type = 0;
 
       for (let i = 0; i < this.input_information.length; i++) {
-        let tmp = parseFloat(this.input_information[i].input_percent);
+        let tmp = -1;
+        tmp = parseFloat(this.input_information[i].input_percent) / 100;
         sum += tmp;
-        if (tmp < 0 || tmp > 1) error_type = 2;
-        if (this.input_information[i].input_name === null) error_type = 3;
+        if (tmp <= 0 || tmp > 1) error_type = 2;
+        if (this.input_information[i].input_name === "" || this.input_information[i].input_name.length === 0) error_type = 3;
       }
       if (this.input_information.length === 1) error_type = 4;
       if (this.abs(sum - 1) > this.epx) error_type = 5;
       if (error_type !== 0) {
-        alert("error" + error_type);
+        if (error_type === 1) alert("收益比例不合法")
+        if (error_type === 2) alert("收益比例不合法")
+        if (error_type === 3) alert("收益方名称不能为空")
+        if (error_type === 4) alert("收益方个数至少为2个")
+        if (error_type === 5) alert("收益比例总和不等于100%")
+        // alert("error" + error_type);
         return;
       }
       this.drawPie("pieChart");
     },
 
     drawPie(id) {
+      this.ctx = "";
       let labels_ = [], data_ = [], backgroundColor_ = [];
       for (let i = 0; i < this.input_information.length; i++) {
         labels_.push(this.input_information[i].input_name);
-        data_.push(this.input_information[i].input_percent)
+        data_.push(parseFloat(this.input_information[i].input_percent) / 100);
         backgroundColor_.push(this.color_lists[i]);
       }
       this.ctx = document.getElementById(id).getContext('2d');
@@ -119,22 +129,5 @@ export default {
 
 
 <style scoped>
-.input-name {
-  width: 50vw;
-}
 
-.input-percent {
-  width: 20vw;
-}
-
-.pie {
-  margin: 0 auto;
-  text-align: center
-}
-
-#pieChart {
-  /*border: solid red 1px;*/
-  width: 30vw;
-  height: 50vh;
-}
 </style>
