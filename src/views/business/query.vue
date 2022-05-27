5b6e21f6907fd44719f2d42912d9b137e3ee53ca
catgok <catgok@163.com>
2022-05-27 12:08:51 +0800

    business/query 70%

--- src/views/business/query.vue
+++ src/views/business/query.vue
@@ -1,46 +1,58 @@
 <template>
   <div>
-    <el-row>
-      <el-col :span="24">
-        <div class="grid-content bg-purple" style="height: 260px">
-          <!-- todo  css -->
-          <ul>
-            <div>商户名称</div>
-            <el-input style="width: 200px" v-model="queryList.name" placeholder="请输入商户名称"></el-input>
-          </ul>
-
-          <ul>
-            <div>商户状态</div>
-            <el-select v-model="queryList.status" placeholder="请选择">
-              <el-option v-for="item in statusList" :key="item.status" :label="item.status"
-                         :value="item.status"></el-option>
-            </el-select>
-          </ul>
-
-          <ul>
-            <div>归属区域</div>
-            <el-select v-model="queryList.status" placeholder="请选择">
-              <el-option v-for="item in addressList" :key="item.address" :label="item.address"
-                         :value="item.address"></el-option>
-            </el-select>
-          </ul>
-
-          <ul>
-            <div>商户层级</div>
-            <el-select v-model="queryList.status" placeholder="请选择">
-              <el-option v-for="item in levelList" :key="item.level" :label="item.level"
-                         :value="item.level"></el-option>
-            </el-select>
-          </ul>
-
-          <el-button @click="search" type="primary" icon="el-icon-search">搜索</el-button>
-          <el-button @click="reset" type="primary" icon="el-icon-refresh">重置</el-button>
-
+    <div class="grid-content bg-purple" style="height: 13vw">
+      <el-row style="display: flex;flex-direction: row">
+        <div class="search-item">商户名称
+          <el-input class="search-item-input" v-model="queryList.name" placeholder="请输入商户名称"></el-input>
         </div>
-      </el-col>
-    </el-row>
 
+        <div class="search-item">商户状态
+          <el-select class="search-item-select" v-model="queryList.status" placeholder="请选择">
+            <el-option v-for="item in statusList" :key="item.status" :label="item.status"
+                       :value="item.status"></el-option>
+          </el-select>
+        </div>
 
+        <div class="search-item">归属区域
+          <el-select class="search-item-select" v-model="queryList.status" placeholder="请选择">
+            <el-option v-for="item in addressList" :key="item.address" :label="item.address"
+                       :value="item.address"></el-option>
+          </el-select>
+        </div>
+      </el-row>
+
+      <el-row style="display: flex;flex-direction: row">
+        <div class="search-item">商户层级
+          <el-select class="search-item-select" v-model="queryList.status" placeholder="请选择">
+            <el-option v-for="item in levelList" :key="item.level" :label="item.level" :value="item.level"></el-option>
+          </el-select>
+        </div>
+        <el-button class="search-item-button" @click="search" type="primary" icon="el-icon-search">搜索</el-button>
+        <el-button class="search-item-button" @click="reset" type="primary" icon="el-icon-refresh">重置</el-button>
+      </el-row>
+
+      <el-row style="display: flex;flex-direction: row">
+        <el-button @click="businessAdd" class="search-item-button" type="primary" plain>新增</el-button>
+        <el-button @click="businessChange" class="search-item-button" type="primary" plain style="color: lawngreen">修改
+        </el-button>
+        <el-button @click="businessCancellation" class=" search-item-button" type="primary" plain style="color: red">注销
+        </el-button>
+      </el-row>
+
+    </div>
+    <div>
+      <div>
+        <el-table ref="singleTable" :data="businessInfoList" highlight-current-row @current-change="handleCurrentChange"
+                  style="width: 100%">
+          <el-table-column type="index" width="50"></el-table-column>
+          <el-table-column property="name" label="商户名称" width="120"></el-table-column>
+          <el-table-column property="status" label="商户状态" width="120"></el-table-column>
+          <el-table-column property="type" label="商户类型" width="120"></el-table-column>
+          <el-table-column property="address" label="地址" width="220"></el-table-column>
+          <el-table-column property="phone" label="联系电话"></el-table-column>
+        </el-table>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -69,6 +81,23 @@ export default {
         level: "",
       },
 
+      businessInfoList: [
+        {
+          name: '玉龙山景区',
+          status: '正常',
+          type: '有限责任公司',
+          address: '云南省丽江市龙纳西族自治县',
+          phone: '13529219181',
+        },
+        {
+          name: '古城驿站区',
+          status: '注销',
+          type: '个体户',
+          address: '云南省丽江市龙纳西族自治县',
+          phone: '13529219181',
+        },
+      ],
+      currentRow: null,
     }
   },
   methods: {
@@ -82,6 +111,22 @@ export default {
     },
     reset() {
       console.log('reset');
+    },
+    setCurrent(row) {
+      this.$refs.singleTable.setCurrentRow(row);
+    },
+    handleCurrentChange(val) {
+      this.currentRow = val;
+    },
+    businessAdd() {
+      this.$router.push('/business/add');
+    },
+    businessChange() {
+      console.log(this.currentRow);
+    },
+    businessCancellation() {
+      console.log(this.currentRow);
+      this.currentRow.status = '注销'
     }
   }
 }
@@ -89,12 +134,13 @@ export default {
 
 <style scoped lang="less">
 .el-row {
-  margin-bottom: 20px;
+  padding-top: 0.8vw;
+  padding-left: 20px;
+  margin-bottom: 0.5vw;
 
   &:last-child {
     margin-bottom: 0;
   }
-
 }
 
 .grid-content {
@@ -105,4 +151,30 @@ export default {
 .bg-purple {
   background: rgb(245, 245, 245);
 }
+
+.search-item {
+  //border: 1px red solid;
+  width: 26vw;
+  height: 3vw;
+}
+
+.search-item-button {
+  //border: 1px red solid;
+  width: 6.5vw;
+  height: 3vw;
+  margin-right: 2vw;
+}
+
+.search-item-select {
+  margin-left: 13px;
+  width: 18vw;
+  height: 3vw;
+}
+
+.search-item-input {
+  margin-left: 13px;
+  width: 18vw;
+  height: 3vw;
+}
+
 </style>
--- src/views/clearing/process.vue
+++ src/views/clearing/process.vue
@@ -1,11 +1,29 @@
 <template>
   <div>clearing process</div>
+  <!--  todo-->
 </template>
 
 <script>
 export default {
   /* eslint-disable */
-  name: "process"
+  name: "process",
+  data() {
+    return {
+      transRecordList: [
+        {
+          trans_sn: "",
+          trans_time: "",
+          trans_amount: "",
+        },
+      ],
+      aa: "",
+    }
+  },
+  methods: {
+    fs() {
+
+    },
+  }
 }
 </script>
 
